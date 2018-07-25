160fbec522ad2b391983471d74328b56a3a615a6
Hyeonseong Jeon <39935435+Cutz-HS@users.noreply.github.com>
2018-07-25 21:05:47 +0900

    Add p195 python

--- p195.py
+++ p195.py
@@ -5,8 +5,7 @@ turtle.title("거북")
 turtle.setup(550,550)
 turtle.screensize(500,500)
 
-allDict={}
-j=0
+allList=[]
 
 shapeList=turtle.getshapes()
 shapeList.remove('blank')
@@ -22,12 +21,27 @@ for i in range(100):
     # size
     tSize=random.randrange(1,4)
     # list
-    allDict[j]=[myTurtle, tX, tY, tSize, r, g, b]
-    j+=1
+    allList.append([myTurtle, tX, tY, tSize, r, g, b])
 
-for value in allDict.values():
-    myTurtle=value[0]
+# sorting
+for i in range(0, len(allList)-1):
+    for j in range(1, len(allList)):
+        if allList[i][1]+allList[i][2]>allList[j][1]+allList[j][2]:
+            allList[i], allList[j]=allList[j], allList[i]
+
+#turtle main
+myTurtle=allList[0][0]
+myTurtle.penup()
+myTurtle.goto(allList[0][1],allList[0][2])
+myTurtle.color((allList[0][4],allList[0][5],allList[0][6]))
+myTurtle.turtlesize(allList[0][3])
+#myTurtle.pendown()
+del(allList[0])
+
+for value in allList:
     myTurtle.color((value[4], value[5], value[6]))
     myTurtle.turtlesize(value[3])
     myTurtle.goto(value[1],value[2])
+    myTurtle.pendown()
+    myTurtle.stamp()
 turtle.done()
